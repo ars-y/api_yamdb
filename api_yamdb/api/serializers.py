5063aeb0f1bee0d3afd04767d301886a7be068c2
ArslanYadov <arsyy90@gmail.com>
2022-08-26 22:17:20 +0300

    add fields for reviewserializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,9 +1,19 @@
+from email.policy import default
 from rest_framework import serializers
 
 from reviews.models import Review, Comment
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    title = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='name'
+    )
     
     class Meta:
         model = Review

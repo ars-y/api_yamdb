9dd767f795406416cca3e2bf4ab4f02e0bd1c9d4
ArslanYadov <arsyy90@gmail.com>
2022-08-26 13:31:16 +0300

    add permission for review & comment

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,11 +1,13 @@
 from rest_framework import viewsets
 
 from reviews.models import Review, Comment
+from api.permissions import AuthorOrReadOnly
 from api.serializers import ReviewSerializer, CommentSerializer
 
 
 class ReviewViewset(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
+    permission_classes = [AuthorOrReadOnly]
 
     def get_queryset(self):
         pass
@@ -16,6 +18,7 @@ class ReviewViewset(viewsets.ModelViewSet):
 
 class CommentViewset(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    permission_classes = [AuthorOrReadOnly]
 
     def get_queryset(self):
         pass

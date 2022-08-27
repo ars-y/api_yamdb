c068acbb5278ef6b71a2d1feaff15622fb935726
ArslanYadov <arsyy90@gmail.com>
2022-08-27 16:39:31 +0300

    general form for all admin models

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -11,31 +11,10 @@ from reviews.models import (
 )
 
 
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    """Админка для ревью."""
-    list_display = (
-        'author',
-        'title',
-        'text',
-        'score',
-        'pub_date',
-    )
-    list_editable = ('title', 'score',)
-    list_filter = ('pub_date',)
-    search_fields = ('author', 'title',)
-
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    """Админка для комментария."""
-    list_display = ('author', 'review', 'text', 'pub_date',)
-    list_editable = ('review',)
-    list_filter = ('pub_date',)
-    search_fields = ('author', 'review',)
-
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Genre)
 admin.site.register(Title)
 admin.site.register(GenreTitle)
+admin.site.register(Review)
+admin.site.register(Comment)

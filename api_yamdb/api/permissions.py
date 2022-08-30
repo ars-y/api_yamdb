9d417d4fbb3a646dd147894dc42cde80ee5e6409
ArslanYadov <arsyy90@gmail.com>
2022-08-30 18:20:56 +0300

    changes in permission for reviews and comments

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -20,25 +20,18 @@ class AdminPermission(permissions.BasePermission):
         )
 
 
-class IsAuthorAdminModerOrReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
+class IsAuthorAdminModerOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
 
     def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.method == 'POST':
+            return request.user.is_authenticated
         return (
-            request.method in permissions.SAFE_METHODS
-            or (
+            request.user.is_authenticated
+            and (
                 obj.author == request.user
-                or (
-                    request.user.is_authenticated
-                    and (
-                        request.user.is_moderator
-                        or request.user.is_admin
-                    )
-                )
+                or request.user.is_moderator
+                or request.user.is_admin
             )
         )
